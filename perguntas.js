5952a9f59d3a41ef3fac601e1663915c0050a096
2DLavinya20 <lillymikaellen@gmail.com>
2024-11-28 16:02:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'série',
+    'Qual é o enredo principal da série e quais são os temas centrais que ela aborda?',
+    '"Meia Noite no Hotel Pera Palace" gira em torno de uma jovem jornalista chamada Esra, que viaja para Istambul e acaba descobrindo um mistério ligado ao hotel Pera Palace, que possui um histórico intrigante. A série mistura elementos de ficção científica, como viagens no tempo, com temas de amor, amizade e a busca pela verdade.'
+)
+
+criaCartao(
+    'Série',
+    'Quem são os personagens principais e qual é o papel de cada um deles na história?',
+    ' Esra: A protagonista, uma jornalista que investiga o passado do hotel. Ahmet:Um misterioso funcionário do hotel que ajuda Esra em sua jornada.  Sofia:Uma mulher do passado que tem uma conexão importante com a história do hotel.  Esses personagens interagem em diferentes períodos de tempo, o que enriquece a narrativa.'
+)
+
+criaCartao(
+    'Série',
+    'Como o hotel Pera Palace é apresentado na série e qual é a sua importância para a trama?',
+    ' O Hotel Pera Palace é retratado como um lugar cheio de história e mistério, com uma atmosfera única. Ele serve como um portal para as viagens no tempo da protagonista e é fundamental para a descoberta dos segredos do passado. '
+)
+
+criaCartao(
+    'Série',
+    'Que elementos de ficção científica ou fantasia estão presentes na narrativa?',
+    'A série incorpora viagens no tempo, permitindo que Esra interaja com personagens do passado e explore eventos históricos enquanto tenta resolver mistérios contemporâneos.'
+)
